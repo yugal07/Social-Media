5dd9bb3b9fb00e797164a51e5aa838c6e0e3a3d4
yugal07 <yashsadhwani544@gmail.com>
2025-05-04 21:39:58 +0530

    More controllers for likes and comments

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -12,6 +12,19 @@ const getAllPosts = async(req , res) => {
     }
 }
 
+const getPostById = async(req , res ) => {
+    try {
+        const { id } = req.params;
+        const post = await Post.findById(id);
+
+        if(!post) return res.status(404).json({message: "Post not found"})
+
+        res.status(200).json(post)
+    } catch(error){
+        console.error(error);
+        res.status(500).json({message: error.message})
+    }
+}
 const createPost = async (req , res) => {
     try {
         const {content , image} = req.body;
@@ -43,7 +56,7 @@ const updatePost = async (req , res) => {
 
         if(!post) return res.status(404).json({message: "Post not found"})
         
-        if(post.author.toString != userId) return res.status(401).json({message: "User unauthorized"})
+        if(post.author.toString() != userId) return res.status(401).json({message: "User unauthorized"})
 
         const updatedPost = await Post.findByIdAndUpdate(id , {content , image} , {new: true})
 
@@ -61,7 +74,7 @@ const deletePost = async (req , res) => {
 
         const post = await Post.findById(id);
         if(!post) return res.status(404).json({message: "Not found"});
-        if(post.author.toString != userId) return res.status(401).json({message: "Unauthorized"})
+        if(post.author.toString() != userId) return res.status(401).json({message: "Unauthorized"})
 
         await Post.findByIdAndDelete(id);
         await User.findByIdAndUpdate(userId , { $pull: {posts: id}})
@@ -71,4 +84,54 @@ const deletePost = async (req , res) => {
     }
 }
 
-module.exports = {createPost , updatePost , getAllPosts , deletePost};
+const likePost = async (req , res) => {
+    try {
+        const postId = req.params.id;
+        const userId = req.user.userId;
+    
+        const post = await Post.findById(postId);
+    
+        if(!post) return res.status(404).json({message: "Post not found"})
+    
+        const alreadyLiked = post.likes.includes(userId);
+    
+        if(alreadyLiked) {
+            post.likes = post.likes.filter((id) => id.toString() != userId);
+        } 
+        else {
+            post.likes.push(userId);
+        }
+    
+        await post.save();
+    
+        res.status(200).json({likes: post.likes , totalLikes: post.likes.length});
+    } catch(error){
+        console.error(error);
+        res.status(500).json({message: error.message})
+    }
+}
+
+const addComment = async (req , res) => {
+    try {
+        const {content} = req.body;
+        const postId = req.params.id;
+        const userId = req.user.userId;
+
+        if(!content) return res.status(404).json({message: "Comment is required"})
+
+        const post = await Post.findById(postId);
+
+        if(!post) return res.status(404).json({message: "Post not found"});
+
+        post.comments.push({content , author: userId});
+
+        await post.save();
+
+        res.status(201).json(post)
+    } catch(error){
+        console.error(error);
+        res.status(500).json({message: error.message})
+    }
+}
+
+module.exports = {createPost , updatePost , getAllPosts , deletePost , getPostById , likePost , addComment};
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const { getAllPosts, createPost, updatePost, deletePost } = require("../controllers/postController");
+const { getAllPosts, createPost, updatePost, deletePost, getPostById, likePost, addComment } = require("../controllers/postController");
+const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+//public routes
 router.get("/" , getAllPosts )
-router.post("/" , createPost);
-router.put("/:id" , updatePost );
-router.delete(":id" , deletePost);
+router.get("/:id" , getPostById)
+
+
+// Private Routes
+router.post("/" ,protect ,  createPost);
+router.put("/:id" , protect , updatePost );
+router.delete("/:id" ,protect , deletePost);
+router.post("/:id/like" , protect , likePost);
+router.post("/:id/comment" , protect , addComment);
 
 module.exports = router;
